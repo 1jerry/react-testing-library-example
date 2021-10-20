e6fa6e25ce67ca9b7416c6d671c0c992ca24bc59
Jerry McRae <git-73ry@sneakemail.com>
2021-10-20 07:47:36 -0700

    add test first

--- src/containers/CommentFeed.test.js
+++ src/containers/CommentFeed.test.js
@@ -1,25 +1,43 @@
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import CommentFeed from "./CommentFeed"
 
-describe("CommentFeed", () => {
-  let comments = [
+const createProps = (props) => ({
+  header: "Comment Feed",
+  comments: [
     {
       author: "Ian Wilson",
       text: "A boats a boat but a mystery box could be anything.",
     },
-    {
-      author: "Max Powers Jr",
-      text: "Krypton sucks.",
-    },
-  ]
-  const props = { header: "Comment Feed", comments }
+    { author: "Max Powers Jr", text: "Krypton sucks." },
+  ],
+  createComments: jest.fn(),
+  ...props,
+})
+describe("CommentFeed", () => {
   it("renders the commentFeed", () => {
-    const { queryByText } = render(<CommentFeed {...props} />)
-    const header = queryByText(props.header)
-    expect(header).toBeTruthy()
-    expect(header.innerHTML).toBe(props.header)
+    // Arrange - create props and locate elements
+    const newComment = { author: "Socrates", text: "Why?" }
+    let props = createProps()
+    const { container, getByLabelText } = render(<CommentFeed {...props} />)
+    const authorNode = getByLabelText("Author")
+    const textNode = getByLabelText("Comment")
+    const formNode = container.querySelector("form")
+
+    // Act - simulate changes to elements
+    fireEvent.change(authorNode, {
+      target: { value: newComment.author },
+    })
+    fireEvent.change(textNode, {
+      target: { value: newComment.text },
+    })
+    fireEvent.submit(formNode)
+
+    // Assert - check whether the desired functions were called
+    expect(props.createComment).toHaveBeenCalledTimes(1)
+    expect(props.createComment).toHaveBeenCalledWith(newComment)
   })
   it("renders the comment list", () => {
+    let props = createProps()
     const { container } = render(<CommentFeed {...props} />)
     const commentNodes = container.querySelectorAll(".Comment")
     expect(commentNodes.length).toBe(props.comments.length)
